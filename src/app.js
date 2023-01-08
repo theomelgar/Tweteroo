b701e50176ad79696b43ab191a117de0808986bb
theomelgar <theomelgar@gmail.com>
2023-01-08 02:34:43 -0300

    tweets/:USERNAME GET

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ let tweets = []
 
 app.post("/sign-up", (req, res) => {
     const user = req.body
-    if(!user || !user.username || !user.avatar ) return res.status(400).send("Todos os campos são obrigatórios!")
+    if (!user || !user.username || !user.avatar || typeof user.username != "string" || typeof user.avatar != "string") return res.status(400).send("Todos os campos são obrigatórios!")
     users.push(user)
     res.status(201).send("OK")
 })
@@ -21,7 +21,7 @@ app.post("/tweets", (req, res) => {
     const newTweet = req.body
     const findUser = users.find(item => item.username == newTweet.username)
     if (findUser) {
-        if(!newTweet || !newTweet.username || !newTweet.tweet) return res.status(400).send("Todos os campos são obrigatórios!")
+        if (!newTweet || !newTweet.username || !newTweet.tweet || typeof newTweet.tweet != "string") return res.status(400).send("Todos os campos são obrigatórios!")
         const id = tweets.length + 1
         const avatar = findUser.avatar
         newTweet.avatar = avatar
@@ -33,8 +33,14 @@ app.post("/tweets", (req, res) => {
 
 })
 
-app.get("/tweets", (req,res) => {
+app.get("/tweets", (req, res) => {
     res.send(tweets.slice(-10).reverse())
+})
+
+app.get("/tweets/:USERNAME", (res, req) => {
+    const filterUser = tweets.filter( tweet => tweet.username === parseInt(req.params.USERNAME));
+    if (!filterUser) return res.send({})
+    res.status(200).send(filterUser);
 })
 
 app.listen(PORT, () => {
